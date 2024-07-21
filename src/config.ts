41131a6453931057594b47ddf620a9a7122cc229
johnisthedeveloper <johnisthedeveloper@gmail.com>
2024-07-21 11:13:18 -0400

    added repo weekly

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+export const SITE = {
+	title: 'GPT AI 周刊',
+	author: 'John',
+	description: '记录工程师 John 的不枯燥生活，每周一发布，欢迎关注',
+	keywords: 'John,Weekly,Blog,前端,macOS,MiaoYan,Pake,Swift,设计',
+	icon: 'https://gw.alicdn.com/imgextra/i2/O1CN01m9YYjS1QBeW5DOm3I_!!6000000001938-2-tps-400-400.png',
+	pic: 'https://gw.alicdn.com/imgextra/i4/O1CN010j3h201UDilmn716N_!!6000000002484-2-tps-1280-640.png',
+	homePage: 'https://weekly.john.fun',
+	blogPage: 'https://john.fun/',
+	twitterId: 'mydailylivemail',
+	githubId: 'johnisthedeveloper',
+	repo: 'johnisthedeveloper/weekly',
+};
